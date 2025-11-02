81e633663e1cb2153f0f1e986bd3cab9487f42e6
Allynn Alvarico <116658923+AllynnAlvarico@users.noreply.github.com>
2025-11-02 22:33:03 +0000

    fix: bugs (change the buttons to anchor tags for easy navigation and to avoid errors)

--- Development Space/RegisterPortal/src/main/java/alvarico/allynn/DatabaseConnection.java
+++ Development Space/RegisterPortal/src/main/java/alvarico/allynn/DatabaseConnection.java
@@ -4,27 +4,30 @@ public class DatabaseConnection {
 
     private String db_schema = "webdev";
 //    private String jdbcConnection = "jdbc:mysql://192.168.178.145:3306/"; local network connection to my database server
-//    private String jdbcConnection = "jdbc:mysql://194.125.24.208:3306/"; outside connection to my database server
+    private String jdbcConnection = "jdbc:mysql://194.125.24.208:3306/"; //outside connection to my database server
 
 //    Main Database user and password
-//    private String db_user = "webdevass1";
-//    private String db_password = "webdevelopmentassignment";
+    private String db_user = "webdevass1";
+    private String db_password = "webdevelopmentassignment";
 
 //    private String db_schema = "tu914";
-    private String jdbc= "jdbc:mysql://localhost:3306/";
+//    private String jdbc= "jdbc:mysql://localhost:3306/";
     private String db_userTable = "user";
-    private String db_user = "root";
+//    private String db_user = "root";
 //    laptop Password
 //    String db_password = "9542MEnw#";
 //    Pc Password
-    String db_password = "@admin2110";
+//    String db_password = "@admin2110";
 
     public String getSchema() {
         return db_schema;
     }
 
+//    public String getJdbc() {
+//        return jdbc;
+//    }
     public String getJdbc() {
-        return jdbc;
+        return jdbcConnection;
     }
 
     public String getTable() {
